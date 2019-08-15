3e77f49534bd2a2eed1992eea16d2055ef778784
Rachel Grier <rgrier104@gmail.com>
2019-08-15 21:55:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @team_members = params[:team][:members]
 
       erb :team
     end
